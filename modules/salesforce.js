015c55329eb0a6f83c3d80fe281f4fec6aa9a119
jaganrnd <jaganrnd@gmail.com>
2016-07-16 00:40:54 +0530

    Update salesforce.js

--- modules/salesforce.js
+++ modules/salesforce.js
@@ -93,7 +93,7 @@ let getTopOpportunities = count => {
 
 let findOpportunities = name => {
     return new Promise((resolve, reject) => {
-        let q = "SELECT Id, Name FROM Opportunity WHERE Name LIKE '%" + name + "%' LIMIT 1";
+        let q = "SELECT Id, Name FROM Opportunity WHERE Name LIKE '%" + name + "%' LIMIT 5";
         org.query({query: q}, (err, resp) => {
             if (err) {
                 reject("An error as occurred");
@@ -108,7 +108,7 @@ let findOpportunities = name => {
 
 let getdummyOpportunities = name => {
     return new Promise((resolve, reject) => {
-        let q = "SELECT Id, Name FROM Opportunity WHERE Name LIKE '%" + name + "%' LIMIT 5";
+        let q = "SELECT Id, Name FROM Opportunity WHERE Name LIKE '%" + name + "%' LIMIT 1";
         org.query({query: q}, (err, resp) => {
             if (err) {
                 reject("An error as occurred");
