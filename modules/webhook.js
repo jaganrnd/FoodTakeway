c60890d428f81ce4f681dddfc7d42db375bd3667
jaganrnd <jaganrnd@gmail.com>
2016-07-15 23:32:28 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -51,7 +51,7 @@ let processText = (text, sender)  => {
     
     match1 = text.match(/Search sunglasses/i);
     if (match1) {
-        salesforce.getTopOpportunities(match[1]).then(opportunities => {
+        salesforce.getdummyOpportunities(match[1]).then(opportunities => {
             sendMessage({text: `Processing your request`}, sender);
             salesforce.sendMessage(formatter.formatModel(opportunities), sender);
         });
