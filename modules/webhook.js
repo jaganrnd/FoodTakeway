01d4e72e6de15f6b5265ba953a9e015d44a1ac43
jaganrnd <jaganrnd@gmail.com>
2016-07-17 15:41:54 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -79,7 +79,7 @@ let processText = (text, sender)  => {
     if (match2) {
         salesforce.findWayfarerOpportunities(match2).then(Opportunities => {    
             sendMessage({text:`Searching Wayfarer Models....`}, sender);
-            sendMessage(formatter.formatWayfarerModels(Opportunities), sender)
+            sendMessage(formatter.formatnewModel(Opportunities), sender)
         });
         return;
     }
