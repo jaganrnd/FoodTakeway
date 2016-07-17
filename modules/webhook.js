b59889d7e7a04aeddd01fa31ab43c352477cb3cf
jaganrnd <jaganrnd@gmail.com>
2016-07-17 12:10:07 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -157,8 +157,6 @@ let handlePost = (req, res) => {
                 sendMessage({text: `OK, I closed the opportunity "${payload[2]}" as "Close Won". Way to go Christophe!`}, sender);
             } else if (payload[0] === "close_lost") {
                 sendMessage({text: `I'm sorry to hear that. I closed the opportunity "${payload[2]}" as "Close Lost".`}, sender);
-            }  else if (payload[0] === "show_square") {
-                sendMessage({text: `Looking Square Models for you`}, sender);
             }
         }
     }
