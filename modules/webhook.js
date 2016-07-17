6935ae9138a11ad80e9b4f8737cf115d31f683ba
jaganrnd <jaganrnd@gmail.com>
2016-07-17 13:30:33 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -77,10 +77,10 @@ let processText = (text, sender)  => {
     let match2;
     match2 = text.match(/Square/i);
     if (match2) {
-        //salesforce.findWayfarerOpportunities(match2[1]).then(Opportunities => {    
+        salesforce.findWayfarerOpportunities(match2[1]).then(Opportunities => {    
             sendMessage({text:`Processing Square Models for you`}, sender);
-            //sendMessage(formatter.formatSquareModels(Opportunities), sender)
-        //});
+            sendMessage(formatter.formatWayfarerModels(Opportunities), sender)
+        });
         return;
     }
     
