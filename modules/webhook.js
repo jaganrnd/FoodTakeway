43b746fb1417db13f1f66a764ea80b105cad1181
jaganrnd <jaganrnd@gmail.com>
2016-07-23 23:38:36 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -164,6 +164,8 @@ let handlePost = (req, res) => {
                 sendMessage({text: `Thanks For Sharing Your Location`}, sender);
                 var lat = event.message.attachments[0].payload.coordinates.lat;
                 var lng = event.message.attachments[0].payload.coordinates.long;
+                alert lat;
+                alert lng;
         } 
         else if (event.postback) {
             let payload = event.postback.payload.split(",");
