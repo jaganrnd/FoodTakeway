f9e45d2f232ab17d62fca75dbbd3fe7daaaba3cb
jaganrnd <jaganrnd@gmail.com>
2016-07-23 17:46:05 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -24,7 +24,7 @@ let sendMessage = (message, recipient) => {
 
 
 //let processLocation = (coordinates.lat,coordinates.long, sender) => {
-let processLocation = (location, sender) => {
+let processLocation = (location33q, sender) => {
     sendMessage({text:`Thanks For Sharing Your Location`}, sender);
 };
 
