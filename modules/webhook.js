11c526345a8269faa0a163d5fbf80abd77e71c3a
jaganrnd <jaganrnd@gmail.com>
2016-07-24 20:18:41 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -181,7 +181,7 @@ let handlePost = (req, res) => {
             sendMessage({text: `Sorry I'm taking a break right now.`}, sender);
         } else if (event.message && event.message.text) {
             processText(event.message.text, sender);
-        }else if (event.message && event.message.attachments) {
+        }else if (event.message && event.message.attachments.type == 'location') {
                 sendMessage({text: `Thanks For Sharing Your Location`}, sender);
                 var lat = event.message.attachments[0].payload.coordinates.lat;
                 var lng = event.message.attachments[0].payload.coordinates.long;
