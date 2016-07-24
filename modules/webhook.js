92279600ddf5ec6c57a8274b6325eab81840b424
jaganrnd <jaganrnd@gmail.com>
2016-07-24 21:46:14 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -179,11 +179,9 @@ let handlePost = (req, res) => {
         let sender = event.sender.id;
         if (process.env.MAINTENANCE_MODE && ((event.message && event.message.text) || event.postback)) {
             sendMessage({text: `Sorry I'm taking a break right now.`}, sender);
-        } 
-        else if (event.message && event.message.text) {
+        }else if (event.message && event.message.text) {
             processText(event.message.text, sender);
-        }
-        else if (event.message && event.message.attachments) {
+        }/*else if (event.message && event.message.attachments) {
                 console.log('Inside Location Loop ', event.message.attachments[0].type);
                 var lat = event.message.attachments[0].payload.coordinates.lat;
                 var lng = event.message.attachments[0].payload.coordinates.long;
@@ -191,7 +189,7 @@ let handlePost = (req, res) => {
                 sendMessage({text: ` Latitude "${lat}" `}, sender);
                 sendMessage({text: ` Latitude "${lng}" `}, sender);
                 //getAddress(lat,lng);
-        } 
+        }*/ 
         else if (event.postback) {
             let payload = event.postback.payload.split(",");
             if (payload[0] === "view_contacts") {
