8b0a2e8ffa21bff21822aec46a30817e58d7d2ac
jaganrnd <jaganrnd@gmail.com>
2016-07-24 20:36:10 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -183,7 +183,7 @@ let handlePost = (req, res) => {
         /*else if (event.message && event.message.text) {
             processText(event.message.text, sender);
         }*/
-        else if (event.message && event.message.attachments.type == 'location') {
+        else if (event.message && event.message.attachments[0].type == 'location') {
                 var lat = event.message.attachments[0].payload.coordinates.lat;
                 var lng = event.message.attachments[0].payload.coordinates.long;
                 if(lat !=NULL && lng !=NULL){
