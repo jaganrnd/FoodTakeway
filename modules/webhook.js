8b1b703ea734dc9289590efa50ed5bdd2857e47b
jaganrnd <jaganrnd@gmail.com>
2016-07-30 21:49:49 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -198,6 +198,10 @@ let handlePost = (req, res) => {
             processText(event.message.text, sender);
         }else if (event.message && event.message.attachments) {
                 console.log('Inside Location Loop ', event.message.attachments[0].type);
+                if(event.message.attachments[0].type == 'location'){
+                    
+                    
+                }
                 //var lat = event.message.attachments[0].payload.coordinates.lat;
                 //var lng = event.message.attachments[0].payload.coordinates.long;
                 //sendMessage({text: `Thanks For Sharing Your Location`}, sender);
