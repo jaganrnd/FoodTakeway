6397cd63a16a0b18c24cc83188ff2c12b312c6ca
jaganrnd <jaganrnd@gmail.com>
2016-07-31 14:30:22 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -254,7 +254,7 @@ let handlePost = (req, res) => {
                 sendMessage({text: `I'm sorry to hear that. I closed the opportunity "${payload[2]}" as "Close Lost".`}, sender);
             } else if(payload[0] === "Order_Now") {
                 sendMessage({text: `Processing your order 🕗`}, sender);
-                salesforce.sendMessage(payload[1]);
+                salesforce.createCase(payload[1]);
             }        
         }
     }
