b4e0db65a0d5fa158cfb601c02809ce9be866c1e
jaganrnd <jaganrnd@gmail.com>
2016-07-31 10:57:48 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -83,7 +83,7 @@ let processText = (text, sender)  => {
 
         //sendMessage({text: `Thanks for your interest, ${response.first_name}. I asked a broker to contact you asap.`}, sender);
 
-        getUserInfo(Sender);
+        getUserInfo(userId);
         
         sendMessage({text:
             `Welcome to the world of Rayban :eyeglasses:
