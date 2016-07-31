3b38d18c88f0be1b1144e8d9074f5fd700bb72ff
jaganrnd <jaganrnd@gmail.com>
2016-07-31 13:58:05 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -253,7 +253,7 @@ let handlePost = (req, res) => {
             } else if (payload[0] === "close_lost") {
                 sendMessage({text: `I'm sorry to hear that. I closed the opportunity "${payload[2]}" as "Close Lost".`}, sender);
             } else if(payload[0] === "Order_Now") {
-                sendMessage({text: `I'm sorry to hear that. I closed the opportunity "${payload[2]}" as "Close Lost".`}, sender);
+                sendMessage({text: `Processing your order`}, sender);
             }        
         }
     }
