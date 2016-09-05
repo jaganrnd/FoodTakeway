5854f146e5896243cb2494c9875ad774321d5417
jaganrnd <jaganrnd@gmail.com>
2016-09-05 16:18:07 +0530

    Update salesforce.js

--- modules/salesforce.js
+++ modules/salesforce.js
@@ -193,7 +193,8 @@ let findMenu = ProductId => {
 let findProductId = MenuId => {
     return new Promise((resolve, reject) => {
         console.log(MenuId);
-        let q = "Select id,name from product2 where  Id IN (SELECT Product__c from Menu__c where id = '" + Menuid + "')";
+        let q = "Select id,name from product2 where  Id IN (SELECT Product__c from Menu__c where id = '" + Menuid + "') LIMIT 1";
+        console.log(q);
         org.query({query: q}, (err, resp) => {
             if (err) {
                 console.log('ERROR');
