69959141ac9f8822c39830225d6db46829fcd0ed
jaganrnd <jaganrnd@gmail.com>
2016-10-02 12:22:58 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -89,7 +89,7 @@ let processText = (text, sender)  => {
             sendMessage({text:
                 `Hey ${response.first_name} !
             "Kolapasi" welcomes you 🙏 🙏
-             Curious to know about me ? 🍴 🍛 Pot of food 🍲
+             Curious to know about me ? 🍲
              Please hit - who are you`
             }, sender);
         
