f32dd78e6b9ea188a0d380731479bae2ce903513
jaganrnd <jaganrnd@gmail.com>
2016-10-03 20:33:52 +0530

    Update formatter-messenger.js

--- modules/formatter-messenger.js
+++ modules/formatter-messenger.js
@@ -1,3 +1,35 @@
 "use strict";
 
 
+let formatTitleCard = Accounts => {
+    let elements = [];
+    Accounts.forEach(Account =>
+        elements.push({
+            title: Account.get("Name"),
+            subtitle: Account.get("Description"),
+            "image_url": Account.get("Picture_URL__c"),
+            "buttons": [
+                {
+                    /*"type": "web_url",
+                    "url": "https://login.salesforce.com/" + opportunity.getId(),
+                    "title": "Buy"*/
+                    
+                    "type":"postback",
+                    "title":"Hit Me",
+                    "payload": "Show_Branches," + Account.getId() + "," + Account.get("Name")
+                    
+                }]
+        })
+    );
+    return {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": elements
+            }
+        }
+    };
+};
+
+exports.formatTitleCard = formatTitleCard;
