4eb2fdaeeec1ef00c2ce862813b997cac228861f
jaganrnd <jaganrnd@gmail.com>
2016-10-05 22:16:18 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -117,12 +117,13 @@ let handlePost = (req, res) => {
                      
                      console.log('Origin Branch - payload [1]**' + payload[1]);    
                      console.log('Origin parent product - payload [2]**' + payload[2]); 
-                     
-		     
+                     sendMessage(formatter.formatQuantity(), sender);  // Hitendar
+		     /*
                      salesforce.findSubMenus(payload[1], payload[2]).then(() => {   
                           console.log('Going inside quantity');
                           sendMessage(formatter.formatQuantity(), sender);  // Hitendar
                      }); 
+		     */
                  } 
                  //Hitendar
         }
