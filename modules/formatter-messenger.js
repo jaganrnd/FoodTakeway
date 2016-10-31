e237e0d008f93ec222cd085271877fd5026b4d84
jaganrnd <jaganrnd@gmail.com>
2016-10-31 23:46:25 +0530

    Update formatter-messenger.js

--- modules/formatter-messenger.js
+++ modules/formatter-messenger.js
@@ -162,13 +162,12 @@ let formatQuantity= SelectedMenu => {
 			"content_type":"text",
 			"title":i,
 			//"payload":"Save_Order," + SelectedMenu[0].get("Product__c") + "," + SelectedMenu[0].get("Quantity__c") + "," + SelectedMenu[0].get("Price__c")
-			//"payload":"Save_Order,"
+			"payload":"Save_Order,"
 		});
 	}
     return {
         "text": "Please select quantity",
          "quick_replies": elements
-	 "payload":"Save_Order,"
     };  
 	
 };
