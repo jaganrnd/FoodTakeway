3eb8efcb54b9e907916bc48e479d57b79770c803
jaganrnd <jaganrnd@gmail.com>
2016-11-02 22:35:31 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -79,7 +79,9 @@ let handlePost = (req, res) => {
 	
     for (let i = 0; i < events.length; i++) {
         let event = events[i];
-        console.log('Events full**' + events[i]);	    
+        
+	console.log('Events full**' + event.message.payload);
+	    
         let sender = event.sender.id;
         if (process.env.MAINTENANCE_MODE && ((event.message && event.message.text) || event.postback)) {
             sendMessage({text: `Sorry I'm taking a break right now.`}, sender);
