311eeb987b7f21eb4e90d76893129b3a49805080
jaganrnd <jaganrnd@gmail.com>
2016-11-09 20:37:55 +0530

    Update salesforce.js

--- modules/salesforce.js
+++ modules/salesforce.js
@@ -194,8 +194,6 @@ let createOpportunityProduct = (ProductId, Price, Quantity) => {
 			console.log('price book entry id stringify*' +    PriceBookEntryId );
 			    
 			console.log('price book entry id stringify*' +    resolve(PriceBookEntryId) );    
-			    
-			console.log('price book entry id separate value*' +   resp.records[0].get(id) );    
 			
 			console.log('price book entry id separate value*' +   resp.records[0].id);	    
 			    
