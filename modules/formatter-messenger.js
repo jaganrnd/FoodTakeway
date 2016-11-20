1cd2c31bc80dd3653ca02e43042eb2c20b98750f
jaganrnd <jaganrnd@gmail.com>
2016-11-20 20:57:43 +0530

    Update formatter-messenger.js

--- modules/formatter-messenger.js
+++ modules/formatter-messenger.js
@@ -201,8 +201,7 @@ let formatShowCart = (SelectedItems) => {
                 },
 		{
                     "type":"web_url",
-		    //"url":"https://www.payumoney.com/pay/#/merchant/367CEAEDF82D367BD2D99C2A064FC7A7?param=Kolapasi",
-		     "url":"http://play.gamemix.com/play?id=42",	
+		    "url":"https://www.payumoney.com/pay/#/merchant/367CEAEDF82D367BD2D99C2A064FC7A7?param=Kolapasi",	
                     "title":"Place Order",
 		    "webview_height_ratio": "compact",	
                      //"messenger_extensions": true,                      
