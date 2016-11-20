5a7261cf7fcb4b5a98beb089727c3218207cea75
jaganrnd <jaganrnd@gmail.com>
2016-11-20 16:11:20 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -62,7 +62,7 @@ function adddomain(){
     } else if (response.body.error) {
         console.log('Error: ', response.body.error);
     } else {
-	 console.log("Success***');    
+	 console.log('Success***');    
     }	    	 	 
 })
 
