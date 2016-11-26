e4094b446f2c07604c025070f4d8398b4b8967ae
jaganrnd <jaganrnd@gmail.com>
2016-11-26 21:11:03 +0530

    Update formatter-messenger.js

--- modules/formatter-messenger.js
+++ modules/formatter-messenger.js
@@ -183,7 +183,7 @@ let formatQuantity= (SelectedMenu,Opportunity, Account, oli) => {
 };
 /* Hitendar */
 
-let formatShowCart = (SelectedItems) => {
+let formatShowCart = (SelectedItems, accountId) => {
   let elements = [];
     SelectedItems.forEach(opportunitylineitem  =>
 			  
@@ -196,7 +196,7 @@ let formatShowCart = (SelectedItems) => {
                 {
                     "type":"postback",
                     "title":"Change Quantity",
-                    "payload": "Change_Quantity,"  +  opportunitylineitem.getId() + "," + opportunitylineitem.get("opportunityid") + "," + opportunitylineitem.get("Opportunity").get("AccountId")
+                    "payload": "Change_Quantity,"  +  opportunitylineitem.getId() + "," + opportunitylineitem.get("opportunityid") + "," + accountId
                    
                 },
 		{
