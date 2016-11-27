470e3c075872df7226c13164b4c77f1f2634095b
jaganrnd <jaganrnd@gmail.com>
2016-11-27 15:46:06 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -110,6 +110,11 @@ let handlePost = (req, res) => {
 			console.log('Normal Flow**');	
 			processText(event.message.text, sender); // WHY KILLING SAMBU MAVAN
 		}
+		if(event.message.text.length  = 10){
+			console.log('Phone Number entered***');	
+			salesforce.updatePhone(event.message.text,sender);
+			sendMessage({text: `Thank you for using this service. Have a good day`}, sender);
+		}
 		
 		if (event.message.quick_reply){
 			
