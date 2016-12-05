065f731aec967089c30d9866b88adc98cdb2fdbf
jaganrnd <jaganrnd@gmail.com>
2016-12-05 18:56:47 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -164,6 +164,28 @@ let handlePost = (req, res) => {
 								    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"                        
 								}
 							    ]
+							},
+							{
+							    "title": "Classic Black T-Shirt",
+							    "image_url": "https://peterssendreceiveapp.ngrok.io/img/black-t-shirt.png",
+							    "subtitle": "100% Cotton, 200% Comfortable",
+							    "default_action": {
+								"type": "web_url",
+								"url": "https://peterssendreceiveapp.ngrok.io/view?item=102",
+								"messenger_extensions": true,
+								"webview_height_ratio": "tall",
+								"fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+							    },
+							    "buttons": [
+								{
+								    "title": "Shop Now",
+								    "type": "web_url",
+								    "url": "https://peterssendreceiveapp.ngrok.io/shop?item=102",
+								    "messenger_extensions": true,
+								    "webview_height_ratio": "tall",
+								    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"                        
+								}
+							    ]                
 							}
 						    ],
 						     "buttons": [
