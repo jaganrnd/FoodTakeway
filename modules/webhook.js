a525390ada81ebbd28cead52ff38a44fd84f501b
jaganrnd <jaganrnd@gmail.com>
2017-04-02 13:00:37 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -511,7 +511,9 @@ let handlePost = (req, res) => {
 			//sendMessage(formatter.formatShareLocation(), sender);  
 			console.log('location Display');
 			sendMessage({text: "Thanks for sharing your number. We will contact you shortly"}, sender);
-			payumoney.paymentFields();
+			payumoney.paymentFields.then(fields =>{
+				console.log('fields***'+fields);
+			});
 		}
 		else if (payload[0] === "Show_Cart"){ 
 			console.log('Show Cart**' + payload[1] );
