6a98c3ab232988646017e0ec06d30c8e843f6cbd
jaganrnd <jaganrnd@gmail.com>
2017-04-03 14:34:12 +0530

    Update formatter-messenger.js

--- modules/formatter-messenger.js
+++ modules/formatter-messenger.js
@@ -348,14 +348,15 @@ let confirmPhone= (phoneNumber) => {
             "buttons": [
                 {
 			//Commented since link is not redirecting via mobile browser
-		    	//"type":"web_url",
-			//"url":"https://test.payu.in/_payment?key=MTeytuI5&txnid=12234445&amount=100&productinfo=test123&surl=test1.com&hash=386007ebcfb8eee2fc32d9ab41930aa7f1dfe7c93d82a5156f9a2e80bede590293af84b5f7b7cf989869a9d6e2109e5e3c75e4662f5c0d4f758b91284bba0202&firstname=hk&email=h@gmail.com&phone=8888888888&furl=test1.com&service_provider=payu_paisa",
+		    	 "type":"web_url",
+			"url":"https://test.payu.in/_payment?key=MTeytuI5&txnid=12234445&amount=100&productinfo=test123&surl=test1.com&hash=386007ebcfb8eee2fc32d9ab41930aa7f1dfe7c93d82a5156f9a2e80bede590293af84b5f7b7cf989869a9d6e2109e5e3c75e4662f5c0d4f758b91284bba0202&firstname=hk&email=h@gmail.com&phone=8888888888&furl=test1.com&service_provider=payu_paisa",
 			//"url":"https://test.payu.in/_payment",
-			//"title":"Proceed to Pay",
-			//"webview_height_ratio": "full"
-			"type":"postback",
-			"title":"Proceed to CheckOut",
-			"payload":"Make_Payment,"
+			"title":"Proceed to Pay",
+			"webview_height_ratio": "full"
+			
+			//"type":"postback",
+			//"title":"Proceed to CheckOut",
+			//"payload":"Make_Payment,"
                     
                 }]
         })
