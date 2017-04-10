0f72620115522d0a9b3eca935ba10408cd6fb831
jaganrnd <jaganrnd@gmail.com>
2017-04-10 16:59:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     webhook = require('./modules/webhook'),
-    success = require('./success'),
     app = express();
 
 app.set('port', process.env.PORT || 5000);
 
 app.use(bodyParser.json());
 
-app.get('/success', success);
+app.get('/success', function (req, res) {
+  res.send('Hello World!')
+})
 
 app.get('/webhook', webhook.handleGet);
 app.post('/webhook', webhook.handlePost);
