3b6e862d8b4ad10244ab5a896937a3d2c9c148be
jaganrnd <jaganrnd@gmail.com>
2017-06-24 16:24:45 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -473,6 +473,8 @@ let handlePost = (req, res) => {
 		else if(payload[3] == 'Feedback/Enquiry'){
 			    sendMessage({text: `Please feel free to Feedback/Enquiry.We are happy to hear from you :) :)`}, sender);  
 			    //sendMessage({text: `🏀`}, sender);    
+			    var userQuery = event.message.text;
+			    console.log('userQuery**' + userQuery);
 		}		
 		//NEWLY ADDED ON 25 - JULY - 2017
 		
