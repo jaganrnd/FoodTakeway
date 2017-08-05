ccead00c3d37283c1a0eb190f53b843a622503b6
jaganrnd <jaganrnd@gmail.com>
2017-08-05 17:12:32 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -483,7 +483,7 @@ let handlePost = (req, res) => {
 		     var obj = { 'parentAccountId': payload[1]};
 	 	     var payloadParameters = JSON.stringify(obj);
 		     console.log('payload parameters'+payloadParameters);
-                     sendMessage({text: "Please share your location to help you with nearest branches. Make sure your Location in ON" 🏂, 
+                     sendMessage({text: "Please share your location to help you with nearest branches. Make sure your Location in ON 🏂", 
 				     quick_replies: [
 				    	 {
 					  "content_type":"location",
