ae208bc3de1c1c6867e8d27eb747d96de67d3ebd
jaganrnd <jaganrnd@gmail.com>
2018-03-25 00:26:42 -0700

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -316,7 +316,7 @@ let handlePost = (req, res) => {
 			//var n = str.startsWith("Hello");
 			
 			//Feedback- Logic
-			if( event.message.text.startsWith("Feedback-") || event.message.text.startsWith("feedback-" || event.message.text.startsWith("Feedback -" ){
+			if( event.message.text.startsWith("Feedback-") || event.message.text.startsWith("feedback-" || event.message.text.startsWith("Feedback -"){
 				console.log('Feeback If Condition Works****');
 				//sendMessage({text: `Good Flow**`}, sender);
 				getUserInfo(sender).then(response => {    
